33da2f686d52cec868e4e8d34023145c14eef0e0
Himanshu Kriplani <himanshukriplani@MacBook-Pro.home>
2020-06-05 00:59:06 -0700

    Used Dragula,Created Drag handler

--- src/Board.js
+++ src/Board.js
@@ -161,4 +161,76 @@ export default class Board extends React.Component {
       </div>
     );
   }
+  componentDidMount() {
+    const swimlanes_containers = Object.values(this.swimlanes).map((lane) => {
+      return lane.current;
+    });
+    this.dragula_object = Dragula(swimlanes_containers);
+
+    this.dragula_object.on("drop", this.handleDrag);
+  }
+  handleDrag = (el, target, par, sibling) => {
+    this.dragula_object.cancel(true);
+    console.log(el, target, par, sibling);
+    let newStatus;
+    switch (target) {
+      case this.swimlanes.inProgress.current:
+        newStatus = "in-progress";
+        break;
+      case this.swimlanes.complete.current:
+        newStatus = "complete";
+        break;
+      case this.swimlanes.backlog.current:
+      default:
+        newStatus = "backlog";
+        break;
+    }
+    const { clients } = this.state;
+
+    const newClientsList = [
+      ...clients.backlog,
+      ...clients.inProgress,
+      ...clients.complete,
+    ];
+    let newIndex = Infinity;
+    let oldIndex = Infinity;
+
+    newClientsList.forEach((cli, idx) => {
+      if (cli.id == el.dataset.id) {
+        oldIndex = idx;
+      }
+      if (sibling && cli.id === sibling.dataset.id) {
+        newIndex = idx - 1;
+      }
+    });
+    // Remove old client
+    const oldClient = newClientsList.splice(oldIndex, 1)[0];
+
+    const updatedClient = {
+      ...oldClient,
+      status: newStatus,
+    };
+    console.log(updatedClient);
+    if (newIndex < oldIndex) newIndex++;
+    newClientsList.splice(
+      newIndex === Infinity ? newClientsList.length : newIndex,
+      0,
+      updatedClient
+    );
+
+    console.log(newIndex, oldIndex);
+    this.setState({
+      clients: {
+        backlog: newClientsList.filter(
+          (client) => !client.status || client.status === "backlog"
+        ),
+        inProgress: newClientsList.filter(
+          (client) => client.status && client.status === "in-progress"
+        ),
+        complete: newClientsList.filter(
+          (client) => client.status && client.status === "complete"
+        ),
+      },
+    });
+  };
 }
